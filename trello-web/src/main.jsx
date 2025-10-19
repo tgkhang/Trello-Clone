0e4f0c673024c8e4e4e9bc94f13026a4f3ad4329
Khang <tgkhang22@clc.fitus.edu.vn>
2025-10-19 18:27:57 +0700

    old experimental apis

--- trello-web/src/main.jsx
+++ trello-web/src/main.jsx
@@ -3,14 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
-import { ThemeProvider } from '@mui/material/styles'
+// import { ThemeProvider } from '@mui/material/styles'
+import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import theme from './theme.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
+    <CssVarsProvider theme={theme}>
       <CssBaseline />
       <App />
-    </ThemeProvider>
+    </CssVarsProvider>
   </StrictMode>,
 )
--- trello-web/src/theme.js
+++ trello-web/src/theme.js
@@ -1,21 +1,47 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
 
-// Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    mode: 'light', // default
-    primary: {
-      main: '#556cd6',
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: {
+          main: '#ff5252',
+        },
+
+      },
     },
-    secondary: {
-      main: '#19857b',
-    },
-    error: {
-      main: red.A400,
+    dark: {
+      palette: {
+        primary: {
+          main: '#000',
+        },
+
+      },
     },
   },
-})
+  // ...other properties
+});
+
+
+// import { createTheme } from '@mui/material/styles'
+// import { red } from '@mui/material/colors'
+
+// Create a theme instance.
+// const theme = extendTheme({
+//   cssVariables: true,
+//   palette: {
+//     mode: 'light', // default
+//     primary: {
+//       main: '#556cd6',
+//     },
+//     secondary: {
+//       main: '#19857b',
+//     },
+//     error: {
+//       main: red.A400,
+//     },
+//   },
+// })
 
 export default theme
