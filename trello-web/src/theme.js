b407ed36066e9eaef05e7799f2d5d9e8deb6ff21
Khang <tgkhang22@clc.fitus.edu.vn>
2025-10-19 17:07:08 +0700

    config_theme

---
+++ trello-web/src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles'
+import { red } from '@mui/material/colors'
+
+// Create a theme instance.
+const theme = createTheme({
+  cssVariables: true,
+  palette: {
+    mode: 'light', // default
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+  },
+})
+
+export default theme
