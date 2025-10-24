534d3e56731788ac6814e4026977f5bf9cd3632c
Khang <tgkhang22@clc.fitus.edu.vn>
2025-10-24 13:18:43 +0700

    small fix

--- trello-web/src/pages/Boards/BoardContent/ListColumns/Columns/ListCards/ListCards.jsx
+++ trello-web/src/pages/Boards/BoardContent/ListColumns/Columns/ListCards/ListCards.jsx
@@ -1,33 +1,38 @@
 import Box from '@mui/material/Box'
 import Card from './Card/Card'
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 function ListCards({ cards }) {
   return (
-    <Box
-      sx={{
-        p: '0 5px',
-        m: '0 5px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 1,
-        overflowX: 'hidden',
-        overflowY: 'auto',
-        maxHeight: (theme) => `calc(
+    //sortable context only use for items type ['id-1', 'id-2'] not an object
+    <SortableContext items={cards?.map(card => card._id)} strategy={verticalListSortingStrategy}>
+      <Box
+        sx={{
+          p: '0 5px',
+          m: '0 5px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          overflowX: 'hidden',
+          overflowY: 'auto',
+          maxHeight: (theme) => `calc(
             ${theme.trello.boardContentHeight} -
             ${theme.spacing(5)} -
             ${theme.trello.columnHeaderHeight} -
             ${theme.trello.columnFooterHeight}
             )`,
-        '&::-webkit-scrollbar-thumb': { backgroundColor: '#ced0da' },
-        '&::-webkit-scrollbar-thumb:hover': { backgroundColor: '#bfc2cf' },
-      }}>
+          '&::-webkit-scrollbar-thumb': { backgroundColor: '#ced0da' },
+          '&::-webkit-scrollbar-thumb:hover': { backgroundColor: '#bfc2cf' },
+        }}>
 
-      {/* List Cards  */}
-      {cards && cards.map((card) => (
-        <Card key={card._id} card={card} />
-      ))}
+        {/* List Cards  */}
+        {cards && cards.map((card) => (
+          <Card key={card._id} card={card} />
+        ))}
 
-    </Box>
+      </Box>
+    </SortableContext>
+
   )
 }
 
