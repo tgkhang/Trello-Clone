b15f3f92dd56d051a8ac85f0273803af81f22586
Khang <tgkhang22@clc.fitus.edu.vn>
2025-10-28 00:58:04 +0700

    Init db connection, config enviroment

--- trello-api/src/config/environment.js
+++ trello-api/src/config/environment.js
@@ -1,1 +1,13 @@
-// 1
+// This file loads environment variables from a .env file and exports them for use in the application.
+// Everytime need to use env variables, just import { env } from '~/config/environment.js'
+import 'dotenv/config'
+
+export const env = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  DATABASE_NAME: process.env.DATABASE_NAME,
+  APP_HOST: process.env.APP_HOST,
+  APP_PORT: process.env.APP_PORT,
+  MONGODB_USER: process.env.MONGODB_USER,
+  MONGODB_PASSWORD: process.env.MONGODB_PASSWORD,
+  AUTHOR: process.env.AUTHOR
+}
