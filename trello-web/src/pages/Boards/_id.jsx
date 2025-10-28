39874e19395c810f1ee2badd270a95fda751caa8
Khang <tgkhang22@clc.fitus.edu.vn>
2025-10-29 00:29:29 +0700

    axios init

--- trello-web/src/pages/Boards/_id.jsx
+++ trello-web/src/pages/Boards/_id.jsx
@@ -2,15 +2,29 @@ import Container from '@mui/material/Container'
 import BoardContent from './BoardContent/BoardContent'
 import BoardBar from './BoardBar/BoardBar'
 import AppBar from '~/components/AppBar/AppBar'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
-  // to do: fetch board data by id from params, check data is complete loading or not
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId= '6900f1c7140c93aced697755'
+
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+
+
+  }, [])
+
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container >
   )
 }
