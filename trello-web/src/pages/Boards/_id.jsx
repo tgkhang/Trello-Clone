f51d75f1b542e591837886d2f567612bd70dab9f
Khang <tgkhang22@clc.fitus.edu.vn>
2025-10-31 02:12:13 +0700

    Add card and collum update state in UI

--- trello-web/src/pages/Boards/_id.jsx
+++ trello-web/src/pages/Boards/_id.jsx
@@ -5,29 +5,49 @@ import AppBar from '~/components/AppBar/AppBar'
 // import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
 import { fetchBoardDetailsAPI, createNewColumnAPI, createNewCardAPI } from '~/apis'
+import { generatePlaceholderCard } from '~/utils/formatter'
+import { isEmpty } from 'lodash'
 
 function Board() {
   const [board, setBoard] = useState(null)
 
   useEffect(() => {
-    const boardId= '690226e2f7a61739bcbae00d'
+    const boardId = '690226e2f7a61739bcbae00d'
 
     fetchBoardDetailsAPI(boardId).then(board => {
+      board.columns.forEach(column => {
+        // drag with empty columns
+        if (isEmpty(column.cards)) {
+          const placeholderCard = generatePlaceholderCard(column)
+          column.cards = [placeholderCard]
+          column.cardOrderIds = [placeholderCard._id]
+        }
+        // console.log(column.cards)
+      })
       setBoard(board)
     })
   }, [])
 
   // this function call api create new column and renew data in state board
   const createNewColumn = async (newColumnData) => {
-    const createdNewColum = await createNewColumnAPI(
-      {
-        ...newColumnData,
-        boardId: board._id
+    const createdNewColumn = await createNewColumnAPI({
+      ...newColumnData,
+      boardId: board._id
+    })
+
+    // New thing need placehodler card for drag and drop with empty column
+    createdNewColumn.cards = [generatePlaceholderCard(createdNewColumn)]
+    createdNewColumn.cardOrderIds = [generatePlaceholderCard(createdNewColumn._id)]
+
+    setBoard(prevBoard => {
+      if (!prevBoard) return prevBoard
+
+      return {
+        ...prevBoard,
+        columns: [...prevBoard.columns, createdNewColumn],
+        columnOrderIds: [...prevBoard.columnOrderIds, createdNewColumn._id]
       }
-    )
-    // renew board data
-
-
+    })
   }
 
   // this function call api create new card and renew data in state board
@@ -38,11 +58,37 @@ function Board() {
         boardId: board._id
       }
     )
-    // renew column data
+    // Renew column data
+    // const newBoard= { ...board }
+    // const columnToUpdate = newBoard.columns.find(column => column._id === createdNewCard.columnId)
+    // if (columnToUpdate)
+    // {
+    //   columnToUpdate.cards.push(createdNewCard)
+    //   columnToUpdate.cardOrderIds.push(createdNewCard._id)
+    // }
+    // setBoard(newBoard)
 
+    setBoard(prev => {
+      if (!prev) return prev
+
+      const updatedColumns = prev.columns.map(column => {
+        if (column._id === createdNewCard.columnId) {
+          return {
+            ...column,
+            cards: [...column.cards, createdNewCard],
+            cardOrderIds: [...column.cardOrderIds, createdNewCard._id]
+          }
+        }
+        return column
+      })
+
+      return {
+        ...prev,
+        columns: updatedColumns
+      }
+    })
 
   }
-
 
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
