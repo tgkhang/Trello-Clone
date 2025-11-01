e0412d9b87d211fa8167a07787b98f6b6d399344
Khang <tgkhang22@clc.fitus.edu.vn>
2025-11-01 11:13:19 +0700

    Setup redux toolkit

--- trello-web/src/redux/store.js
+++ trello-web/src/redux/store.js
@@ -1,1 +1,10 @@
 // Redux tool kit
+import { configureStore } from '@reduxjs/toolkit'
+import { activeBoardReducer } from './activeBoard/activeBoardSlice'
+
+export const store = configureStore({
+  reducer: {
+    activeBoard: activeBoardReducer
+    //... add more reducers here later if needed
+  },
+})
